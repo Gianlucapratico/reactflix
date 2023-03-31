12516d4cf97be3e545b63ab150d4e5b5b22539ad
Gianlucapratico <123976541+Gianlucapratico@users.noreply.github.com>
2023-03-31 16:36:26 +0200

    compito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import Film1 from "./components/Film1";
+import Film2 from "./components/Film2";
+import Film3 from "./components/Film3";
+import MyFooter from "./components/MyFooter";
+import TopBar from "./components/TopBar";
+import TvShows from "./components/TvShows";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container bg-dark">
+      <TopBar />
+      <TvShows />
+      <Film1 />
+      <Film2 />
+      <Film3 />
+      <MyFooter />
     </div>
   );
 }
